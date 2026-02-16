79233757548ab5b96596e9cf324d255e75c2dbf3
rev dan wa mariam <dangimwas456@gmail.com>
2026-02-16 13:16:41 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/reverend-dan-connect/",
-
+  base: "/Rev-dan-wa-Mariam/",
   server: {
     host: "::",
     port: 8080,
